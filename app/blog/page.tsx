2929d8b65c447c15d302412ed551bbe88a926752
tsukaryu <drgon8otsuka@gmail.com>
2024-05-03 15:59:15 +0900

    fix: todo追加

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -5,6 +5,7 @@ export default function BlogPage() {
     <section className="min-h-screen">
       <div className="flex flex-col gap-4 mt-16">
         <h1 className="font-semibold text-3xl">Blog</h1>
+        {/* TODO: 全文検索機能 */}
         <p className="leading-7">
           I will be writing about various topics, including programming, team
           management, book reviews, and diary entries. Additionally, there is a
