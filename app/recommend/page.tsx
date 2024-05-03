3c2d3abde94876fc1ab69a09b7e525b7805a78fd
tsukaryu <drgon8otsuka@gmail.com>
2024-05-03 23:57:22 +0900

    feat: add page

---
+++ app/recommend/page.tsx
@@ -0,0 +1,10 @@
+export default function RecommendPage() {
+  return (
+    <section className="min-h-screen flex flex-col gap-8">
+      <div className="flex flex-col gap-4 mt-16">
+        <h1 className="font-semibold text-3xl">Recommend</h1>
+        <p className="leading-7">My favorite things.</p>
+      </div>
+    </section>
+  );
+}
