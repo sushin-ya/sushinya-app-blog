6bf9d0e3d0e4c65ead75b2434477f0f699272dfe
tsukaryu <drgon8otsuka@gmail.com>
2024-05-03 12:50:05 +0900

    feat: topのロゴとページ名

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,70 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+// circle付きのアイコン
+function SushinyaIcon() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <svg
+      width="80"
+      height="80"
+      viewBox="0 0 500 500"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="250" cy="250" r="250" fill="#262626" />
+      <path
+        d="M292.732 109.672C298.311 109.672 304.727 110.788 311.98 113.019C319.233 114.972 325.092 118.32 329.555 123.062C332.624 125.852 332.205 126.689 328.3 125.573C324.394 125.015 320.349 124.178 316.165 123.062C312.259 121.667 307.936 120.97 303.193 120.97C285.898 120.691 273.344 125.712 265.533 136.034C257.722 146.076 253.259 158.909 252.143 174.53C251.306 185.41 251.446 196.289 252.562 207.169C253.957 217.769 255.491 228.37 257.165 238.971C258.838 249.571 260.233 260.172 261.349 270.772C262.744 281.373 262.883 292.113 261.767 302.992C261.488 306.061 261.07 307.874 260.512 308.432C260.233 308.99 259.815 308.85 259.257 308.013C258.978 307.177 258.559 306.061 258.001 304.666C257.722 302.992 257.444 301.597 257.165 300.481C255.491 289.323 253.399 278.165 250.888 267.006C248.377 255.569 246.006 244.131 243.774 232.694C241.543 221.256 239.729 209.819 238.335 198.382C237.219 186.944 237.219 175.367 238.335 163.651C239.172 156.119 240.985 149.145 243.774 142.729C246.564 136.034 250.191 130.315 254.654 125.573C259.396 120.551 264.975 116.646 271.392 113.856C277.808 111.067 284.921 109.672 292.732 109.672ZM218.249 164.906C219.086 157.374 205.557 142.31 208.625 134.778C211.694 126.968 231.221 127.525 236.242 121.388C241.543 114.972 231.082 98.5745 238.335 94.948C245.867 91.0426 270.973 100.466 280.179 100.466C280.458 100.466 281.155 100.606 282.271 100.885C283.666 100.885 284.782 101.024 285.619 101.303C286.734 101.582 287.153 101.861 286.874 102.14C286.874 102.419 285.898 102.698 283.945 102.977C276.413 104.372 269.439 106.882 263.023 110.509C256.607 114.135 251.027 118.599 246.285 123.899C241.543 129.199 237.637 135.197 234.569 141.892C231.779 148.308 230.105 155.003 229.547 161.977C228.432 175.367 228.989 188.897 231.221 202.566C233.453 215.956 235.963 229.346 238.753 242.736C241.822 256.127 244.332 269.656 246.285 283.325C248.517 296.715 249.075 310.106 247.959 323.496C247.401 328.796 246.146 334.375 244.193 340.233C242.24 346.092 239.59 351.531 236.242 356.553C233.174 361.574 229.268 365.898 224.526 369.524C219.784 372.872 214.483 374.964 208.625 375.801C204.162 376.359 202.209 376.219 202.767 375.383C203.604 374.267 205.278 373.011 207.788 371.617C217.831 366.037 224.666 359.482 228.292 351.95C232.198 344.418 234.708 335.073 235.824 323.914C236.94 311.082 236.382 297.971 234.15 284.581C232.198 271.191 229.826 257.8 227.037 244.41C224.247 231.02 221.736 217.769 219.505 204.658C217.552 191.268 217.134 178.017 218.249 164.906ZM206.952 396.723C218.668 396.723 228.571 394.631 236.661 390.447C244.751 385.983 251.446 380.264 256.746 373.29C262.046 366.037 266.091 357.808 268.881 348.602C271.671 339.118 273.484 329.354 274.321 319.311C275.437 306.758 275.158 294.205 273.484 281.652C271.81 269.098 269.857 256.685 267.626 244.41C265.673 231.857 263.86 219.304 262.186 206.75C260.512 194.197 260.233 181.504 261.349 168.672C262.465 155.84 266.37 145.658 273.065 138.126C280.039 130.594 290.361 126.828 304.03 126.828C308.772 126.828 314.77 127.665 322.023 129.339C329.555 131.012 335.692 133.384 340.435 136.452C345.456 139.8 345.037 140.776 339.179 139.381C335.832 138.544 331.508 137.568 326.207 136.452C320.907 135.336 316.862 134.778 314.073 134.778C303.193 134.778 293.848 137.15 286.037 141.892C278.226 146.634 273.763 155.003 272.647 166.998C271.252 179.831 271.531 192.663 273.484 205.495C275.437 218.048 277.668 230.741 280.179 243.573C282.69 256.406 284.782 269.238 286.455 282.07C288.408 294.902 288.827 307.734 287.711 320.567C286.874 331.167 284.782 341.489 281.434 351.531C278.087 361.574 273.344 370.64 267.207 378.73C261.07 386.82 253.399 393.376 244.193 398.397C235.266 403.139 224.666 405.51 212.391 405.51C209.044 405.51 204.72 404.952 199.42 403.837C194.119 402.721 188.819 401.326 183.519 399.652C178.218 397.699 173.197 395.468 168.455 392.957C163.991 390.168 160.644 387.238 158.412 384.17C155.902 381.38 156.738 380.683 160.923 382.078C165.665 384.588 171.384 387.657 178.079 391.283C185.053 394.91 194.677 396.723 206.952 396.723ZM204.441 382.078C211.415 382.078 217.831 380.962 223.689 378.73C229.547 376.498 235.545 373.569 241.682 369.943V371.198C241.682 373.709 240.287 376.219 237.498 378.73C234.987 380.962 231.779 383.054 227.874 385.007C223.968 386.959 219.505 388.494 214.483 389.61C209.741 390.725 204.859 391.283 199.838 391.283C197.885 391.283 195.235 391.004 191.888 390.447C188.54 389.61 185.053 388.633 181.427 387.517C178.079 386.123 174.871 384.588 171.802 382.915C168.734 381.241 166.642 379.288 165.526 377.056C162.457 373.709 161.481 371.896 162.597 371.617C163.713 371.059 165.247 371.477 167.2 372.872C171.663 374.267 176.684 376.219 182.263 378.73C188.122 380.962 195.514 382.078 204.441 382.078Z"
+        fill="#FAFAFA"
+      />
+    </svg>
+  );
+}
+
+function ArrowIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 12H19"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M12 5L19 12L12 19"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default function TopPage() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <div className="flex flex-col gap-6">
+        <SushinyaIcon />
+        <div className="flex flex-col  gap-4">
+          <h1 className="text-5xl font-extrabold">
+            Sushin-ya <br />
+            Blog
+          </h1>
+          {/* TODO: あとでリンク先を実装 */}
+          <Link href="" className="inline-block">
+            <div className="flex items-center gap-4">
+              <p>Read more about me</p>
+              <ArrowIcon />
+            </div>
+          </Link>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
