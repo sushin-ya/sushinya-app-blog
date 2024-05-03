100e5eae4bb4ec17d82c5c45437ef4d6fad04ccc
tsukaryu <drgon8otsuka@gmail.com>
2024-05-03 15:28:26 +0900

    feat: BlogPageを追加

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+export default function BlogPage() {
+  return (
+    <section className="min-h-screen">
+      <div className="flex flex-col gap-4 mt-16">
+        <h1 className="font-semibold text-3xl">Blog</h1>
+        <p>
+          I will be writing about various topics, including programming, team
+          management, book reviews, and diary entries. Additionally, there is a
+          full-text search feature available.
+        </p>
+      </div>
+    </section>
+  );
+}
