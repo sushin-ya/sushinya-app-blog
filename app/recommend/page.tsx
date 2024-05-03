ea369529e11d0e156559808d999ce3ff15f837a9
tsukaryu <drgon8otsuka@gmail.com>
2024-05-04 00:12:33 +0900

    feat: recommend

--- app/recommend/page.tsx
+++ app/recommend/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-
 export default function RecommendPage() {
   return (
     <section className="min-h-screen flex flex-col gap-8">
       <div className="flex flex-col gap-4 mt-16">
         <h1 className="font-semibold text-3xl">Recommend</h1>
         <p className="leading-7">My favorite things.</p>
+        {/* TODO: 好きなものリストを作って画像のurlを取得すること */}
       </div>
       <div className="columns-2 md:columns-3 lg:columns-4">
         {Array.from({ length: 20 }).map((_, index) => (
