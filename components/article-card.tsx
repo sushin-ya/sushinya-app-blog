4168d29ee7df0f50e7af8e42d9b424d7d6399ab8
tsukaryu <drgon8otsuka@gmail.com>
2024-05-03 16:34:25 +0900

    feat: cardの画像を追加

--- components/article-card.tsx
+++ components/article-card.tsx
@@ -1,10 +1,19 @@
 import { Card, CardContent, CardFooter } from "./ui/card";
+import Image from "next/image";
 
 export function ArticleCard() {
   return (
     <Card>
-      <CardContent>画像</CardContent>
-      <CardFooter>タグとか</CardFooter>
+      <CardContent className="p-0">
+        <Image
+          src="/dummy.png" // TODO:　あとで修正
+          width="496"
+          height="270"
+          alt="Image"
+          className="rounded-t-md object-cover"
+        />
+      </CardContent>
+      <CardFooter className="text-neutral-400">タグとか</CardFooter>
     </Card>
   );
 }
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border border-neutral-700 bg-card bg-black text-neutral-50 shadow-sm",
       className
     )}
     {...props}
